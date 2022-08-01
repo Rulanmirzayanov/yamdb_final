c4c76625676bc84f6c2826a9e9190a86fce005a2
Руслан Мирзаянов <rus.mirzayanov@yandex.ru>
2022-08-01 23:26:11 +0300

    isort2

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -7,7 +7,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from reviews.models import Category, Genre, Review, Title, User
+from ..reviews.models import Category, Genre, Review, Title, User
 
 from .filters import TitleFilter
 from .permissions import (AuthorModerAdminOrReadOnly, IsAdminUserOrReadOnly,
