17c93a1cb0611ce509b85b56784c56c9dec6a3e1
Руслан Мирзаянов <rus.mirzayanov@yandex.ru>
2022-08-01 23:16:12 +0300

    isort

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,6 +1,8 @@
 import os
-from dotenv import load_dotenv
 from datetime import timedelta
+
+from dotenv import load_dotenv
+
 load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
